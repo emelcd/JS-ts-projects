e3d730f55ef198a9d90df36a6a5ba9e46db29e3b
emelcd <mick.altura@gmail.com>
2022-06-18 03:44:16 +0200

    Adjust values

--- color-flipper/src/index.ts
+++ color-flipper/src/index.ts
@@ -5,28 +5,33 @@ const green = document.querySelector<HTMLInputElement>("#green")!;
 const blue = document.querySelector<HTMLInputElement>("#blue")!;
 const color = document.querySelector<HTMLSpanElement>("#color")!;
 
-function valueToHex(n: number) {
+/* Using toString to mod  */
+function valueToHex(n: number): string {
   if (n > 255) n = 255;
   if (n < 0) n = 0;
   const hex = n.toString(16);
   return hex.length === 1 ? "0" + hex : hex;
 }
 
-function rgbToHex(r: number, g: number, b: number) {
+function rgbToHex(r: number, g: number, b: number): string {
   return "#" + valueToHex(r) + valueToHex(g) + valueToHex(b);
 }
 
 const handleChange = () => {
-  const nwColor = rgbToHex(red.valueAsNumber, green.valueAsNumber, blue.valueAsNumber);
+  const nwColor = rgbToHex(
+    red.valueAsNumber,
+    green.valueAsNumber,
+    blue.valueAsNumber
+  );
   color.style.color = nwColor;
   color.innerHTML = nwColor;
   document.body.style.backgroundColor = nwColor;
+  console.log(red.valueAsNumber, green.valueAsNumber, blue.valueAsNumber);
 };
 [red, green, blue].forEach((input) => {
   input.addEventListener("input", handleChange);
 });
 
-
 window.addEventListener("load", () => {
   handleChange();
-})
+});
